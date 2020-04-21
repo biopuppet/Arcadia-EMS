c1b6809abb8d1c1f5840c0577dfaa8da48e1172f
liu fengbo <demard@buaa.edu.cn>
2020-04-21 13:42:06 +0800

    chore: Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='arcadia',
+  version='1.0',
+  packages=find_packages(),
+  description="arcadia package",
+  author='biopuppet',
+  author_email='biopuppet@outlook.com',
+  url="None",
+)
