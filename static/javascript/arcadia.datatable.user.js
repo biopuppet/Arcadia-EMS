f3067796d425ed9f1a36a26ccc2435aab81d90e7
liu fengbo <demard@buaa.edu.cn>
2020-04-30 16:49:34 +0800

    fix(user): Update user datatable stuff.

--- static/javascript/arcadia.datatable.user.js
+++ static/javascript/arcadia.datatable.user.js
@@ -1,34 +1,13 @@
-function initDatatable(table_id, url_data_table) {
-    function addRound() {
-        $(".dataTables_paginate > .pagination").addClass("pagination-rounded")
-    }
-
-    function trunc(type, data, max) {
-        return type === 'display' && data.length > max ? data.substr(0, max) + '...' : data;
-    }
-
-    $('#'+table_id).DataTable({
-        "responsive": true,
-        "processing": false, // I'm blind, for christ's sake..
-        "serverSide": false, // Frontend job
-        "ajax": {
-            "url": url_data_table,
+function initDatatable(table, ajax_url) {
+    table.DataTable($.extend(true, {}, CONSTANT.DATA_TABLE.DEFAULT_OPTION, {
+        ajax: {
+            "url": ajax_url,
             "type": "POST",
             "data": {"csrfmiddlewaretoken": jQuery("[name=csrfmiddlewaretoken]").val()},
             "dataSrc": "",
         },
         autoWidth: false,
         columns: [
-            /*
-            {
-                title: "<input type='checkbox' name='checkbox' class='checkboxes'/>",
-                "data": "null",
-                width: "1%",
-                searchable: false,
-                "render": function (data, type, full, meta) {
-                    return "<td> <input type='checkbox' name='checkbox' class='checkboxes' ></td>";
-                }
-            },*/
             {
                 title: 'ID',
                 data: 'id',
@@ -70,8 +49,7 @@ function initDatatable(table_id, url_data_table) {
                 title: 'Phone',
                 data: 'phone',
                 responsivePriority: 4,
-                width: "15%",
-
+                width: "10%",
             },
             {
                 title: 'Department',
@@ -93,53 +71,30 @@ function initDatatable(table_id, url_data_table) {
                     return content;
                 }
             },
-            {
-                title: "Action",
-                data: null,
-                searchable: false,
-                orderable: false,
-                className: "table-action text-center",
-                responsivePriority: 0,
-                width: "5%",
-                "render": function (data, type, full, meta) {
-                    var view_btn = "<a href='/user/" + full.id + "' class='action-icon'>" +
-                        "<i class='mdi mdi-eye'></i></a>";
-                    var update_btn = "<a href='#' class='action-icon' data-toggle='modal' " +
-                        "data-target='#modal-update-user' onclick='send_update_user_id(" + full.id + ")'>" +
-                        "<i class='mdi mdi-square-edit-outline'></i></a>";
-                    var more_btn =
-                        "<a class='action-icon ' href='#' id='dropdownMenuLink' data-toggle='dropdown'>" +
-                        "<i class='mdi mdi-dots-horizontal'></i></a>" +
-                        "<div class='dropdown-menu' aria-labelledby='dropdownMenuLink'>" +
-                        "<a href='/user/toggle-user-status/" + full.id + "' class='dropdown-item'>" +
-                        "Toggle status</a></div>";
-                    return view_btn + update_btn + more_btn;
-                }
-            },
-
+            CONSTANT.DATA_TABLE.COLUMN.ACTION,
         ],
-
-        // keys: {
-        //     columns: ':not(:last-child, :first-child)',
-        // },
-        stateSave: true,
-        scrollCollapse: true,
-        scrollX: false,
-        //"scrollXInner": "120%",
-        // scrollX: true,
-        //scrollCollapse: 0,
-        pagingType: "full_numbers",
-        // select: {style: "single"},
-        language: {
-            paginate: {
-                previous: "<i class='mdi mdi-chevron-left'>",
-                next: "<i class='mdi mdi-chevron-right'>"
-            }
-        },
-        drawCallback: addRound,
-        // createdRow: function (a, i, e) {
-        //     15e4 < 1 * i[5].replace(/[\$,]/g, "") && $("td", a).eq(5).addClass("text-danger")
-        // }
-    });
+        columnDefs: [
+            {
+                "targets": -1,
+                title: "Action",
+                "render":
+                    function (data, type, full, meta) {
+                        var view_btn = "<a href='/user/" + full.id + "' class='action-icon'>" +
+                            "<i class='mdi mdi-eye'></i></a>";
+                        var update_btn = "<a href='#' class='action-icon' data-toggle='modal' " +
+                            "data-target='#modal-update-user' onclick='send_update_user_id(" + full.id + ")'>" +
+                            "<i class='mdi mdi-square-edit-outline'></i></a>";
+                        var more_btn =
+                            "<a class='action-icon ' href='#' id='dropdownMenuLink' data-toggle='dropdown'>" +
+                            "<i class='mdi mdi-dots-horizontal'></i></a>" +
+                            "<div class='dropdown-menu' aria-labelledby='dropdownMenuLink'>" +
+                            "<a href='/user/toggle-user-status/" + full.id + "' class='dropdown-item'>" +
+                            "Toggle status</a></div>";
+                        return view_btn + update_btn + more_btn;
+                    },
+            },
+        ],
+    }))
 }
+
 
